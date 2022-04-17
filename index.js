a8623630db3dc681fb49514f932b3fc57a25fa62
MayceAlazzawi <maynab4@gmail.com>
2022-04-17 17:19:55 +0300

    donee

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = batteryBatches.reduce(myFunc);
+
+function myFunc(a, b) {
+  return a + b;
+}
+
+console.log(totalBatteries)
+
+
+
+
